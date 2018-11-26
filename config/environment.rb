59ac384a081139ccfcc64dad18a97ea9f1b75318
Cone <carlos.gutierrez@magmalabs.io>
2018-11-26 17:23:50 -0600

    Redirecting console output to STDOUT

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# For watching Active Record work under the covers on the console
+# by redirecting the output to standard output. Use like:
+# >> log_to STDOUT
+# >> Post.find(:first)
+def log_to(stream)
+  ActiveRecord::Base.logger = Logger.new(stream)
+  ActiveRecord::Base.clear_active_connections!
+end
