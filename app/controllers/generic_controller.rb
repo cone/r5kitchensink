eac26a5d41230cc18b00178c6b84a8c0a11b150c
Cone <coneramu@gmail.com>
2019-11-25 16:19:25 -0600

    Example of using prepend_before_action

--- app/controllers/generic_controller.rb
+++ app/controllers/generic_controller.rb
@@ -1,9 +1,10 @@
 class GenericController < ApplicationController
-  before_action :set_variable, only: :callbacks_test
+  before_action :modify_var, only: :callbacks_test
   before_action do
     @callback_var += " and also this one. "
   end
   after_action R5kitchensink::TestCallback
+  prepend_before_action :set_variable # executes before any of the 'before_action' callbacks
 
   def callbacks_test
     render plain: "the variable set by the callback is = #{@callback_var}"
@@ -13,5 +14,9 @@ class GenericController < ApplicationController
 
   def set_variable
     @callback_var = "This is a variable set by the callback"
+  end
+
+  def modify_var
+    @callback_var += ' MODIFIED'
   end
 end
