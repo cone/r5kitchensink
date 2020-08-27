ccbf28716b0bb9e8538434679c1220ea1960cdd8
Carlos Gutierrez <coneramu@gmail.com>
2020-08-26 19:47:10 -0500

    Example of some manual Active record configurations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
+  # self.table_name = 'PRODUCT'
+  # self.primary_key = 'MYID'
+
   belongs_to :brand
 end
---
+++ config/initializers/legacy_settings.rb
@@ -0,0 +1,9 @@
+# Rails.application.config.active_record.pluralize_table_names = false
+
+# Rails.application.config.active_record.primary_key_prefix_type = :table_name
+# Or
+# Rails.application.config.active_record.primary_key_prefix_type = :table_name_with_underscore
+
+# Rails.application.config.active_record.table_name_prefix = 'before_table_name'
+
+# Rails.application.config.active_record.table_name_suffix = 'after_table_name'
