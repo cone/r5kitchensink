a67639a7317061881db3e3264ee8bef31ca147a2
Carlos Gutierrez <coneramu@gmail.com>
2020-08-26 20:14:06 -0500

    Add default values and extra data to a model's attributes

--- app/models/dummy.rb
+++ app/models/dummy.rb
@@ -1,3 +1,18 @@
 class Dummy < ApplicationRecord
+  # defining a default value using rails 5 attribute API
+  attribute :name, :string, default: 'n/a'
+
   has_many :tests
+
+  # how we would do it in older versions of rails
+  # def name
+  #   read_attribute(:name) || 'n/a'
+  #   # or: self[:name] || 'n/a'
+  # end
+
+  # how to add some extra text in all names
+  # def name=(value)
+  #   write_attribute(:name, value + ' is the name')
+  #   # or self[:name] = value + ' is the name'
+  # end
 end
